af5677590b30b3d0dcaeb735b89da8323eee62fa
guqinghuan <gqhpww@126.com>
2015-07-13 21:14:52 +0800

    提交初始化代码

---
+++ src/com/yuanda/linphone/util/YDFeatureOptions.java
@@ -0,0 +1,6 @@
+package com.yuanda.linphone.util;
+
+public class YDFeatureOptions {
+    public static final boolean PASSWORD_MD5_STORE_SUPPORT = true;
+    public static final String YD_SIP_SERVER_ADDRESS = "120.26.204.162";
+}
